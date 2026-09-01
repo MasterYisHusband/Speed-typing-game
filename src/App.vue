b2353a05830233f58eebe301d32390d6f4842ac8
gillmann <lea.gillmann@chamaeleon.de>
2026-09-01 12:39:05 +0200

    Css hinzugefügt, Falsch und Richtig + farbe, Input etc.

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const text: string = 'Hello'
+
+const userInput = ref('')
+</script>
+
+<template>
+  <div v-for="(character, index) in text.split('')">
+    <span
+      :class="{
+        correct: text[index] === userInput[index],
+        wrong: userInput[index] !== undefined && text[index] !== userInput[index],
+      }"
+      >{{ character }}</span
+    >
+  </div>
+
+  <input v-model="userInput" />
+  <p>{{ userInput }}</p>
+</template>
+
+<style>
+.correct {
+  color: green;
+}
+
+.wrong {
+  color: red;
+}
+</style>

9cf2e147cebc0f08d2dfe512da35e2952b1b394e
gillmann <lea.gillmann@chamaeleon.de>
2026-09-02 11:11:45 +0200

    Für WPM eine History hinzugefügt.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const text: string = 'Hello'
+const text: string = 'Hallo'
 
 const userInput = ref('')
 
@@ -11,16 +11,21 @@ const isRunning = ref(false)
 
 const timeLeft = ref(30)
 
+const history = ref<number[]>([])
+
 function startGame() {
+  if (isRunning.value === true) {
+    return
+  }
   isRunning.value = true
 
   const timer = setInterval(() => {
     timeLeft.value--
-
     if (timeLeft.value === 0) {
       clearInterval(timer)
       isRunning.value = false
-      wpm.value = userInput.value.split(' ').length / 0.5
+      wpm.value = userInput.value.split(' ').length / (30 / 60)
+      history.value.push(wpm.value)
       timeLeft.value = 30
       userInput.value = ''
     }
@@ -28,6 +33,7 @@ function startGame() {
 }
 </script>
 
+<!-- Template -->
 <template>
   <div v-for="(character, index) in text.split('')">
     <span
@@ -46,9 +52,12 @@ function startGame() {
     {{ timeLeft }}
   </div>
 
-  <p>{{ wpm }}</p>
+  <div>
+    <p v-for="(character, index) in history">Round {{ index + 1 }}: {{ character }} WPM</p>
+  </div>
 </template>
 
+<!-- CSS -->
 <style>
 .correct {
   color: green;

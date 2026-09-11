a83f3816774adf15ecf65f10fe4e2720164ccbeb
gillmann <lea.gillmann@chamaeleon.de>
2026-09-11 10:38:03 +0200

    Fix Github Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/speed-typing-game/',
+  base: '/Speed-typing-game/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
